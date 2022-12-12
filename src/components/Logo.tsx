660edde83b7579f7355503b542894dc6c5c91fd1
Ivan <ivan.hdk@gmail.com>
2022-12-12 19:10:40 +0100

    logo gradient

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import useTheme from '@mui/material/styles/useTheme';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
@@ -8,10 +9,16 @@ import Typography from '@mui/material/Typography';
  * Renders simple logo in header
  */
 const Logo: React.FC = () => {
+  const theme = useTheme();
   return (
     <Box>
       <Link href="/" underline="none" color="inherit" >
-        <Typography component="h1" variant="h4" sx={{ whiteSpace: 'nowrap', lineHeight: 1.5 }}>Movie Database</Typography>
+        <Typography component="h1" variant="h4" sx={{
+          whiteSpace: 'nowrap', lineHeight: 1.5,
+          background: theme.palette.background.fancy,
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent"
+        }}>Movie Database</Typography>
       </Link>
     </Box>
   )
