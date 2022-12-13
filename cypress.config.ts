592cb9a500aa0604f045059f95875bf060f3d71d
Ivan <ivan.hdk@gmail.com>
2022-12-13 17:17:54 +0100

    test url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000/", //"https://ihdk-movie-database.netlify.app/",
+    baseUrl: "https://ihdk-movie-database.netlify.app/",
     env: {
       appNameText: "Movie Database",
     }
